23f3aaf904916a28277e3d540930e4a71d4e1d44
Zhafir18 <m.zhafirghiffari@gmail.com>
2024-03-14 12:28:04 +0700

    Initial commit

---
+++ src/main/java/com/PemiluDigital/PemiluDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.PemiluDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemiluDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PemiluDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PemiluDigital/PemiluDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PemiluDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemiluDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
